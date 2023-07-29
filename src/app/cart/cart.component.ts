dadc72ba54eaa00d42302adb0343db2d7b79aaf4
Alexandre Trieste <alexandre.trieste@gmail.com>
2023-07-29 03:37:07 +0000

    feat: validation form and webstorage

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 import { CartService } from '../cart.service';
 
@@ -8,24 +8,52 @@ import { CartService } from '../cart.service';
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
 
   items = this.cartService.getItems();
 
   checkoutForm = this.formBuilder.group({
-    name: '',
-    address: ''
-  });
+    name: ['', [Validators.required, Validators.pattern('^(?!\\s)[A-Za-zÀ-ÿ ]*$')]],
+    address: ['', [Validators.required, Validators.pattern('^(?!\\s)[A-Za-z0-9,À-ÿ ]*$')]]
+  });  
+
+  entity: any;
 
   constructor(
     private cartService: CartService,
     private formBuilder: FormBuilder,
   ) {}
 
+  ngOnInit() {
+    // Do something here after the component is initialized.
+  }
+
   onSubmit(): void {
     // Process checkout data here
     this.items = this.cartService.clearCart();
-    console.warn('Your order has been submitted', this.checkoutForm.value);
-    this.checkoutForm.reset();
+
+    const nameValue = this.checkoutForm.get('name')?.value;
+    const addressValue = this.checkoutForm.get('address')?.value;
+
+    if (nameValue && addressValue) {
+      const entity = {
+        name: nameValue,
+        address: addressValue,
+      };
+
+      localStorage.setItem('entity', JSON.stringify(entity));
+      console.warn('Your order has been submitted', this.checkoutForm.value);
+      this.checkoutForm.reset();
+    }
+  }
+
+  get isNameInvalid(): boolean {
+    const nameControl = this.checkoutForm.get('name');
+    return nameControl!.invalid && (nameControl!.dirty || nameControl!.touched);
+  }
+
+  get isAddressInvalid(): boolean {
+    const addressControl = this.checkoutForm.get('address');
+    return addressControl!.invalid && (addressControl!.dirty || addressControl!.touched);
   }
 }
